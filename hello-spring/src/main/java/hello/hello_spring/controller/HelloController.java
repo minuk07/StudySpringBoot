24b1f26509b98bfc65fba1b80b0f520c238881f5
minuk07 <lmw3164@naver.com>
2025-01-24 17:23:00 +0900

    1-24_api 사용 예시

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,4 +24,32 @@ public class HelloController {
         return "hello-template"; //hello-template.html을 찾아감. (templates 폴더안에)
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody //http의 response Body에 return 내용을 직접 넣어주겠다.
+    public String helloString(
+            @RequestParam("name") String name
+    ){
+        return "hello " + name; //hello name이 client에게 문자가 그대로 내려감.
+    }
+
+    @GetMapping("/hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello; //객체를 넘김. -> Json으로 반환.
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
